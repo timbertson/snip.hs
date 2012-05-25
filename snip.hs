4cf4feb6b28b395eb6f2633d45fd753242b6c3f0
gfxmonk <tim@gfxmonk.net>
2012-05-25 23:44:37 +1000

    .

--- snip.hs
+++ snip.hs
@@ -1,20 +1,26 @@
 #!/usr/bin/runhaskell
 {-
 
-Directory-tree-printing example: slow version
-Tom Moertel <tom@moertel.com>
-2007-03-20
+Copyright (C) 2012 Tim Cuthbertson
 
-See http://blog.moertel.com/articles/tag/directory_tree_series
-(Look for Part 3)
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
 
-Compile:  ghc -O2 -o tlist-slow --make tlist-slow.hs
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
 
-Usage: ./tlist-slow [directory...]
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-NOTE:  If you run this program on a large directory hierarchy,
-       prepare to wait.  It takes 0.1 second to process each
-       file-system node it encounters.    (See fsVisit, below.)
+
+Original directory-scanning code from:
+http://blog.moertel.com/articles/tag/directory_tree_series
+(C) Tom Moertel <tom@moertel.com> 2007
+(Licenced under the GPL)
 
 -}
 
@@ -29,6 +35,7 @@ import System.FilePath
 import System.Directory
 import System.Environment
 import System.IO
+import System.Process (readProcess)
 
 import System.IO.Unsafe
 
@@ -44,7 +51,7 @@ type DirTree    = Tree DirName     -- file-system tree
 
 data Item = Folder [DirName] | File String deriving Show
 
--- High-level program logic:  get args and print a tree for each
+-- High-level program logic: process args and perform the appropriate action
 
 main :: IO ()
 main = do
@@ -63,30 +70,38 @@ getOrElse Nothing b = b
 getOrElse (Just a) _ = a
 
 type Action = (QueryPath -> DirTree -> IO ())
+type LookupAction = (TreePath, DirTree) -> IO ()
 
 parseArgs :: [String] -> (Action, QueryPath)
 parseArgs [] = (actionHelp, [])
 parseArgs args@(x:xs)
-	| x == "get"  = (actionGet, xs)
-	| x == "new"  = (actionNew, xs)
-	| x == "set"  = (actionSet, xs)
-	| x == "echo" = (actionGet, xs)
-	| x == "all" = (actionAll, xs)
-	| otherwise = (actionGet, args)
+	| x == "get"   = (lookupAction actionGet, xs)
+	| x == "new"   = (actionNew, xs)
+	| x == "set"   = (actionSet, xs)
+	| x == "copy"  = (actionCopy, xs)
+	| x == "all"   = (actionAll, xs)
+	| x == "rm"    = (lookupAction actionRemove, xs)
+	| x == "which" = (lookupAction actionWhich, xs)
+	| otherwise    = (lookupAction actionGet, args)
 
-actionGet :: Action
-actionGet search tree = (getValue search tree) >>= (putStrLn . pp)
+actionGet :: LookupAction
+actionGet node = getSnippetContents node >>= putStrLn . pp
 
 actionCopy :: Action
-actionCopy search tree = (getValue search tree) >>= printAndCopy
+actionCopy search tree = (getValue search tree) >>= copyToClipboard search
 
 actionSet :: Action
 actionSet args tree = do
 	let relpath = init args
-	let val = last args
+	val <- getInput $ last args
 	let path = addToRoot tree relpath
 	writeFile path val
 	putStrLn $ "Got it. " ++ (joinPath relpath) ++ " is now: " ++ val
+
+getInput "-" = do
+	putStrLn "(reading from stdin, ctrl+d to finish)"
+	getContents
+getInput val = return val
 
 actionNew :: Action
 actionNew args tree = do
@@ -94,21 +109,61 @@ actionNew args tree = do
 	createDirectory path
 	putStrLn $ "Created: " ++ (joinPath args)
 
+actionWhich :: LookupAction
+actionWhich = putStrLn . joinPath . fst
+
+actionRemove :: LookupAction
+actionRemove (path, _) = do
+			let path' = joinPath path
+			putStrLn $ "really delete" ++ path' ++ "? [Y/n]"
+			answer <- getLine
+			when (answer `elem` ["","y","Y"]) (putStrLn $ "TODO: remove" ++ path')
+
+lookupAction :: LookupAction -> Action
+lookupAction action search tree = success `getOrElse` (fail $ "Could not find " ++ (describeSearch search))
+	where
+		success = fmap action (resolvePath search tree)
+
 actionAll :: Action
 actionAll [] tree = do
-	putStr $ show tree
+	putStr $ showTree tree
 actionAll args _ = fail "too many arguments for `all`"
 
 actionHelp :: Action
-actionHelp args tree = putStrLn $ ("Your lists:" ++ (pp (Folder (map rootLabel (subForest tree)))))
+actionHelp args tree = putStrLn $ ("Your lists:\n" ++ (pp (Folder (map rootLabel (subForest tree)))))
+
+-- action helpers / workers
 
 addToRoot tree path = joinPath ([rootLabel tree] ++ path)
+
+copyToClipboard search item = do
+	output <- readProcess "pyperclip" ["--copy"] (up item)
+	putStr output -- should be empty, but just in case?
+	putStrLn $ "Copied " ++ (describeSearch search) ++ " to your clipboard."
+
+describeSearch :: QueryPath -> String
+describeSearch = intercalate ">"
+
+getValue :: TreePath -> DirTree -> IO Item
+getValue path tree = success `getOrElse` (fail $ "Could not find " ++ (describeSearch path))
+	where success = (fmap getSnippetContents) (resolvePath path tree)
+
+getSnippetContents :: (TreePath, DirTree) -> IO Item
+getSnippetContents match@(path, tree) =
+	(doesDirectoryExist (joinPath path)) >>= \isDir ->
+		if isDir
+			then return $ Folder (map rootLabel (subForest tree))
+			else do
+				let fullPath = (joinPath path)
+				-- putStrLn $ "reading file: " ++ fullPath
+				contents <- readFile fullPath
+				return $ File contents
 
 
 -- pretty print
 pp :: Item -> String
 pp (Folder []) = "(empty list)"
-pp (Folder contents) = "\n - " ++ (intercalate "\n - " contents)
+pp (Folder contents) = " - " ++ (intercalate "\n - " contents)
 pp (File contents) = contents
 
 -- ugly print
@@ -116,32 +171,11 @@ up :: Item -> String
 up (Folder contents) = intercalate "\n" contents
 up (File contents) = contents
 
-printAndCopy item = do
-	putStrLn (pp item)
-	copyToClipboard (up item)
 
-copyToClipboard s = putStrLn "TODO: copy stuff to clipboard!"
+-- tree traversal / searching
 
-getValue :: TreePath -> DirTree -> IO Item
-getValue path tree = success `getOrElse` (fail $ "Could not find " ++ (intercalate ">" path))
-	where success = (fmap Main.getContents) (resolvePath path tree)
-
-getContents :: (TreePath, DirTree) -> IO Item
-getContents match@(path, tree) =
-	(doesDirectoryExist (joinPath path)) >>= \isDir ->
-		if isDir
-			then return $ Folder (map rootLabel (subForest tree))
-			else do
-				let fullPath = (joinPath path)
-				putStrLn $ "reading file: " ++ fullPath
-				contents <- readFile fullPath
-				return $ File contents
-
-traverseAndPrint :: Path -> IO ()
-traverseAndPrint path =
-	putStr . showTree =<< fsTraverse root
-	where
-		root = if "/" `isPrefixOf` path then "" else "."
+firstMatch :: [Maybe a] -> Maybe a
+firstMatch items = join $ find isJust items
 
 resolvePath :: TreePath -> DirTree -> Maybe (TreePath, DirTree)
 resolvePath path tree = foldl resolveOne (Just ([rootLabel tree], tree)) path
@@ -152,9 +186,9 @@ resolveOne start needle = join $ fmap applyFind start
 		applyFind :: (TreePath, DirTree) -> Maybe (TreePath, DirTree)
 		applyFind = uncurry (findBF' needle)
 
+
 -- find node by name in a breadth-first manner.
 -- returns the path to the node, and the subtree at that point
-
 findBF :: DirName -> DirTree -> Maybe (TreePath, DirTree)
 findBF needle tree = findBF' needle [] tree
 findBF' :: DirName -> TreePath -> DirTree -> Maybe (TreePath, DirTree)
@@ -172,39 +206,25 @@ findBF' needle prefix tree = topLevelMatch <|> findNextLevel
 returnTree :: TreePath -> DirTree -> (TreePath, DirTree)
 returnTree prefix node = (ncons prefix node, node)
 
+-- path manipulation
+
 ncons :: TreePath -> DirTree -> TreePath
 ncons xs x = xs `pcons` (rootLabel x)
 
 pcons :: TreePath -> String -> TreePath
 pcons xs x = xs ++ [x]
 
-firstMatch :: [Maybe a] -> Maybe a
-firstMatch items = join $ find isJust items
-
-
-stringy :: DirTree -> Tree String
-stringy = fmap show
-
 -- Effectful tree-builder for file-system hierarchies
--- dirsep = "/"
--- joinPath lst = intercalate dirsep lst
+
 joinOne a b = joinPath [a,b]
 
 fsTraverse :: Path -> IO DirTree
--- fsTraverse = curry (lazyUnfoldTreeM fsTraverseStep)
 fsTraverse path = (lazyUnfoldTreeM fsTraverseStep) (path, path)
 
 fsTraverseStep :: DirNode -> IO (DirName, [DirNode])
-fsTraverseStep dnode@(path, node) = do
-	name <- fsVisit dnode
-	children <- fsGetChildren (joinOne path node)
+fsTraverseStep dnode@(path, name) = do
+	children <- fsGetChildren (joinOne path name)
 	return (name, children)
-
--- Helper to visit a directory node via an "expensive" operation
-
-fsVisit :: DirNode -> IO DirName
-fsVisit (_, name) = do
-	return name
 
 
 -- Helper to get traversable directory entries
@@ -214,23 +234,8 @@ fsGetChildren path = do
 	contents <- getDirectoryContents path `catch` const (return [])
 	let visibles = sort . filter (`notElem` [".", ".."]) $ contents
 	let isDir name = doesDirectoryExist $ joinOne path name
-	-- (dirs, files) <- partitionM isDir visibles
 	-- print visibles
 	return (map ((,) path) visibles)
-
--- PartitionM - apply Data.List.Partition for a predicate of type (M Bool)
-
--- partitionM :: Monad m => (a -> m Bool) -> [a] -> m ([a],[a])
--- partitionM p xs = do
--- 	(f,g) <- pMHelper p xs
--- 	return (f [], g [])
--- 	where
--- 		pMHelper :: Monad m => (a -> m Bool) -> [a] -> m ([a] -> [a],[a] -> [a])
--- 		pMHelper p xs = foldM help (id,id) xs
--- 			where
--- 				help (f,g) x = do
--- 				b <- p x
--- 				return (if b then (f . (x:),g) else (f,g . (x:)))
 
 lazyUnfoldTreeM :: (b -> IO (a, [b])) -> b -> IO (Tree a)
 lazyUnfoldTreeM step seed = do
@@ -240,26 +245,14 @@ lazyUnfoldTreeM step seed = do
 		mapM (lazyUnfoldTreeM step) seeds
 	return (Node root children)
 
--- purely functional "finder" functions
-
--- find :: DirTree -> Maybe DirNode
--- find t = 
-
 -- Purely functional tree-to-string formatting
 
 showTree :: Tree String -> String
-showTree t = unlines (showNode "" "" "" t)
+showTree t = unlines (concat $ map (showNode "") (subForest t))
 
-showNode :: String -> String -> String -> Tree String -> [String]
-showNode leader tie arm node =
-	nodeRep : showChildren node (leader ++ extension)
+showNode :: String -> Tree String -> [String]
+showNode leader node =
+	nodeRep : childRep
 	where
-		nodeRep   = leader ++ arm ++ tie ++ rootLabel node
-		extension = case arm of "" -> ""; "`" -> "    "; _   -> "|   "
-
-showChildren :: Tree String -> String -> [String]
-showChildren node leader =
-	let
-		children = subForest node
-		arms = replicate (length children - 1) "|" ++ ["`"]
-	in concat (zipWith (showNode leader "-- ") arms children)
+		nodeRep  = leader ++ " - " ++ rootLabel node
+		childRep = concat $ map (showNode (leader ++ "  ")) (subForest node)
